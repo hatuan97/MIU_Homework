b410a9792ef797b42a7d3483713d256095f0b5d9
AnhTuanHa <tuanha17897@gmail.com>
2024-07-25 21:53:52 -0500

    update name

--- src/MPP/Lesson9_part2/prob11b/Main.java
+++ src/MPP/Lesson9_part2/prob11b/Main.java
@@ -10,7 +10,7 @@ public class Main {
 				          new Employee("John", "Sims", 110000),
 				          new Employee("Joe", "Stevens", 200000),
 		                  new Employee("Andrew", "Reardon", 80000),
-		                  new Employee("Joe", "Cummings", 760000),
+		                  new Employee("Joe", "Biden", 760000),
 		                  new Employee("Steven", "Walters", 135000),
 		                  new Employee("Thomas", "Blake", 111000),
 		                  new Employee("Alice", "Richards", 101000),
